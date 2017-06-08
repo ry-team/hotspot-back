b70c1c3d52f971703ececb1dbb1a01f1b86b9258
Ruslan Yuldashev <monitoringe@gmail.com>
2017-06-08 17:37:09 +0500

    Stub API

--- hotspot/apps/api/urls.py
+++ hotspot/apps/api/urls.py
@@ -7,10 +7,10 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'sms', views.SMSView.as_view(), 'sms')
+router.register(r'^sms', views.SMSView, 'sms')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^sms', include(router.urls)),
+    url(r'^', include(router.urls)),
 ]
--- hotspot/apps/api/views.py
+++ hotspot/apps/api/views.py
@@ -1,5 +1,33 @@
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSetMixin
 
 
-class SMSView(APIView):
-    base_name = 'sms'
+class SMSView(ViewSetMixin, APIView):
+    @list_route(methods=('post',))
+    def send(self, request):
+        phone_number = request.data.get('number')
+        internet_type = request.data.get('type')
+
+        if not phone_number:
+            return Response({'status': 'error', 'msg': 'Invalid number'})
+
+        if internet_type not in ['0', '1']:
+            return Response({'status': 'error', 'msg': 'Invalid type'})
+
+        return Response({'status': 'success'})
+
+
+    @list_route(methods=('post',))
+    def check(self, request):
+        phone_number = request.data.get('number')
+        code = request.data.get('code')
+
+        if not phone_number:
+            return Response({'status': 'error', 'msg': 'Invalid number'})
+
+        if code != '0':
+            return Response({'status': 'error', 'msg': 'Invalid code'})
+
+        return Response({'status': 'success'})
